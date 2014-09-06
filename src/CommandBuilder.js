1dec9a4583b945d742db42aad44b7c0bec1d7a7b
Alvaro Garcia <derecerca@gmail.com>
2014-09-06 10:30:21 +0200

    green

--- src/CommandBuilder.js
+++ src/CommandBuilder.js
@@ -2,5 +2,19 @@ function buildCommand(commandNameAndArguments){
 	if(!commandNameAndArguments){
 		return {};
 	}
-	return "["+commandNameAndArguments[0]+(commandNameAndArguments[1]?":"+commandNameAndArguments[1]:"")+"]";
+	var parameters = "";
+	if(commandNameAndArguments.length > 0){
+		for (var i = 0; i < commandNameAndArguments.length; i++) {
+			if(i === 0){
+				parameters += commandNameAndArguments[i];
+			} else if(i === 1){
+				parameters += ":"+commandNameAndArguments[i];
+			} else if(i === commandNameAndArguments.length - 1){
+				parameters += "-"+commandNameAndArguments[i];
+			} else {
+				parameters += "-"+commandNameAndArguments[i];
+			}
+		};
+	}
+	return "["+parameters+"]";
 }
