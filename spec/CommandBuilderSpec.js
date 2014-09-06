df57902acd36a98651ef41e7809d452d2d7f01e9
Alvaro Garcia <derecerca@gmail.com>
2014-09-06 10:18:34 +0200

    red: Command Builder should format a command name without parameters

--- spec/CommandBuilderSpec.js
+++ spec/CommandBuilderSpec.js
@@ -2,4 +2,8 @@ describe("Command Builder", function(){
 	it("should exist", function(){
 		expect(buildCommand()).toBeTruthy();
 	});
+
+	it("should format a command name without parameters", function(){
+		expect(buildCommand(["make"])).toEqual("[make]");
+	});
 });
